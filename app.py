e909a04cc6a24c9ece6b0a3d16cedddd40fdf621
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2020-11-19 11:22:29 +0100

    reproduced

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 import psutil
 import os
 from celery import Celery
@@ -23,26 +24,29 @@ def get_mem_usage():
     print(f"memory usage: {n}")
     objgraph.show_growth(limit=3)
 
+
 def gen_big_str():
     return "x" * 1000 * 1000 * random.randint(1, 10)
 
 
-def run_task():
-    args = ({'foo': gen_big_str(), 'bar': [{'bla': gen_big_str()}]},)
-    return app.send_task('hello', args=args, chain=[Signature('hello')])
+def is_ready(result):
+    return result.ready()
 
 
 if __name__ == "__main__":
     LEAK = True
     with concurrent.futures.ThreadPoolExecutor() as executor:
         get_mem_usage()
-        for _ in range(20):
+        for _ in range(10):
             for _ in range(10):
-                r = run_task()
+                args = ({'foo': gen_big_str(), 'bar': [{'bla': gen_big_str()}]},)
                 if LEAK:
-                    future = executor.submit(r.get)
-                    assert future.result()
+                    future = executor.submit(app.send_task, 'hello', args=args, chain=[Signature('hello')])
+                    result = future.result()
+                    while not executor.submit(is_ready, result).result():
+                        time.sleep(0.001)
                 else:
-                    assert r.get()
+                    result = app.send_task('hello', args=args, chain=[Signature('hello')])
+                assert result.get()
             gc.collect()
             get_mem_usage()
